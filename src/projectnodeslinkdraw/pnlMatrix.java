73c3f2e0ba5ceabbcc1d18178a69cd5680a52a70
Adityaag00 <adityaagrawal399@gmail.com>
2021-02-02 01:30:24 +0530

    Update

--- src/projectnodeslinkdraw/pnlMatrix.java
+++ src/projectnodeslinkdraw/pnlMatrix.java
@@ -12,49 +12,58 @@ import javax.swing.JPanel;
 
 public class pnlMatrix extends JPanel implements Runnable{
 
-    FrmDikstra dik;
+    FrmAnimation dik;
     
-    public pnlMatrix(FrmDikstra dik) {
+    public pnlMatrix(FrmAnimation dik) {
         this.dik=dik;
         setPreferredSize(new Dimension(500, 720));
     }
     
-    public void paintComponent(Graphics g) {
+    public void paintDikstra(Graphics g) {
         g.setColor(Color.darkGray);
         g.fillRect(0, 0, 500, 720);
         g.setColor(Color.BLACK);
         
-        for(int i=0;i<dik.dist.length+1;i++) {
+        for(int i=0;i<dik.dik.dist.length+1;i++) {
             g.drawLine(20, 30*i, 120, 30*i);
         }
         
-        g.drawLine(20, 20, 20, dik.dist.length*30);
-        g.drawLine(120, 20, 120, dik.dist.length*30);
+        g.drawLine(20, 20, 20, dik.dik.dist.length*30);
+        g.drawLine(120, 20, 120, dik.dik.dist.length*30);
         
         g.setFont(new Font("Monospaced", Font.BOLD, 22));
         int j=30;
-        for(int i=1;i<dik.graph.length;i++) {
+        for(int i=1;i<dik.dik.graph.length;i++) {
             g.setColor(Color.WHITE);
             String s="";
-            if(dik.dist[i]==Integer.MAX_VALUE) {
+            if(dik.dik.dist[i]==Integer.MAX_VALUE) {
                 s=String.valueOf(Character.toChars(Integer.parseInt("221E", 16)));;
             }
             else {
-                s=""+dik.dist[i];
+                s=""+dik.dik.dist[i];
             }
             g.drawString(s, 32, j+=30);
         }
         g.setFont(new Font("Monospaced", Font.BOLD, 12));
-        g.drawString("Distance From Source", 20, dik.dist.length*30+35);
-        for(int i=1;i<dik.dist.length;i++) {
+        g.drawString("Distance From Source", 20, dik.dik.dist.length*30+35);
+        for(int i=1;i<dik.dik.dist.length;i++) {
             String s="Distance from 1 to "+i+" is ";
-            if(dik.dist[i]==Integer.MAX_VALUE) {
+            if(dik.dik.dist[i]==Integer.MAX_VALUE) {
                 s+=String.valueOf(Character.toChars(Integer.parseInt("221E", 16)));
             }
             else {
-                s+=""+dik.dist[i];
+                s+=""+dik.dik.dist[i];
             }
-            g.drawString(s, 20, dik.dist.length*30+35+(i*25));
+            g.drawString(s, 20, dik.dik.dist.length*30+35+(i*25));
+        }
+    }
+    
+    public void paintComponent(Graphics g) {
+        switch(dik.dr.algoSelected) {
+            case 1:
+                paintDikstra(g);
+                break;
+            case 2:break;
         }
     }
     
